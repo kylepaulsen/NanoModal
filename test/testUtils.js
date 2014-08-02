76d29c61c0e8ab153f9e7fafafc8999d1d0fc709
Kyle Paulsen <kyle.a.paulsen@gmail.com>
2014-08-01 17:06:15 -0700

    Adding more unit tests for other modules.

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,25 @@
-function touchClick(el) {
-    var eventType = "ontouchend" in document.documentElement ? "touchstart" : "click";
+if (!require) {
+    var require = function(src) {
+        var noop = function() {};
+        var sources = {
+            "./ModalEvent": window.ModalEvent || noop
+        };
+        var requiredFunction = sources[src];
+        if (requiredFunction === noop) {
+            console.warn("Could not find required module: " + src);
+        }
+        return requiredFunction;
+    }
+}
+
+if (!module) {
+    var module = {};
+}
+
+function touchClick(el, eventType) {
+    if (!eventType) {
+        eventType = "ontouchend" in document.documentElement ? "touchstart" : "click";
+    }
     var clickEvent = document.createEvent('MouseEvent');
     clickEvent.initMouseEvent(eventType, true, true, window,
         0, 0, 0, 0, 0, false, false, false, false, 0, null);
